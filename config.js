ae5d035f8cb440657deda38723d8d8018acccf4c
Anukool-film <Anukool.kli@gmail.com>
2026-06-24 17:31:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// ตั้งค่า URL ปลายทางเก็บ log การสุ่ม (Google Apps Script Web app)
+// ทุกเครื่องที่เปิดเว็บจะส่ง log เข้า Google Sheet เดียวกันอัตโนมัติ
+window.LOG_URL = "https://script.google.com/macros/s/AKfycbwlULA8K7_ef96nV1jVTjUUFCMbZTdzGJkOOtOs7BSP9ofsETPF-0Codw01jyVW4AVP/exec";
